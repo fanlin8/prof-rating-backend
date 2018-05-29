b0a7f268a835af5c996dc72cb82671264059d5a8
Erick Lin <erick_lin@ao.uscourts.gov>
2018-05-29 12:40:10 -0400

    Added mongoDB atlas config.

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,18 @@
-module.exports = {
-    url: 'mongodb://localhost:27017/hu-rating'
-}
+require('dotenv').config();
+const mongoose = require("mongoose");
+
+const dbURI = process.env.DB_URI;
+console.log(process.env.PORT);
+
+const options = {
+  reconnectTries: 10,
+  poolSize: 10
+};
+
+// Connecting to the database
+mongoose.connect(dbURI, options)
+  .then(() => {
+    console.log("Successfully connected to the database");
+  }).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+  });
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
 
 // create express app
 const app = express();
+
+// Configuring the database
+const dbConfig = require('./config/database.config.js');
+const port = process.env.PORT || 9090;
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,48 +15,35 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 
-// Configuring the database
-const dbConfig = require('./config/database.config.js');
-const mongoose = require('mongoose');
-
-// Connecting to the database
-mongoose.connect(dbConfig.url)
-.then(() => {
-    console.log("Successfully connected to the database");    
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...');
-    process.exit();
-});
-
 // define a simple route
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to HU Rating application. This is a project based Angular framework."});
+  res.json({ "message": "Welcome to HU Rating application. This is a project based Angular framework." });
 });
 
 // Add headers
 app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Pass to next layer of middleware
-    next();
+  // Pass to next layer of middleware
+  next();
 });
 
 // require Professors routes
 require('./app/routes/professor.routes.js')(app);
 
 // listen for requests
-app.listen(9090, () => {
-    console.log("Server is listening on port 9090");
+app.listen(port, () => {
+  console.log(`Server is listening on port: ${port}`);
 });
